ea9769cd70661f9cbee817220ea2474909d10108
Montana Gau <montanagau13@gmail.com>
2018-07-22 23:48:17 -0500

    finished tutorial

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,11 +5,16 @@ import { MessageService } from "./message.service";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { catchError, map, tap } from "rxjs/operators";
 
+const httpOptions = {
+	headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 	private heroesUrl = 'api/heroes'; // URL to web api
+
 
   constructor(
 	  private messageService: MessageService,
@@ -31,6 +36,45 @@ export class HeroService {
 			tap(_ => this.log(`fetched hero id=${id}`)),
 			catchError(this.handleError<Hero>(`getHero id=${id}`))
 		)
+  }
+
+  /** PUT: update the hero on the server. */
+  updateHero(hero: Hero): Observable<any> {
+		return this.http.put(this.heroesUrl, hero, httpOptions)
+		.pipe(
+			tap(_ => this.log(`updated hero id=${hero.id}`)),
+			catchError(this.handleError<any>('updateHero'))
+		);
+  }
+
+  /** POST: add a new hero to the server. */
+  addHero(hero: Hero): Observable<Hero> {
+	return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+		tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+		catchError(this.handleError<Hero>('AddHero'))
+	);
+  }
+
+  /** DELETE: delete the hero from the server. */
+  deleteHero(hero: Hero): Observable<any> {
+	const id = typeof hero === 'number' ? hero: hero.id;
+	const url = `${this.heroesUrl}/${id}`;
+
+	return this.http.delete<Hero>(url, httpOptions).pipe(
+		tap(_ => this.log(`deleted hero id=${id}`)),
+		catchError(this.handleError<Hero>('deleteHero'))
+	);
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+	if(!term.trim()) {
+		// if not search term, return empty array.
+		return of([]);
+	}
+	return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+		tap(_ => this.log(`found heroes matching "${term}"`)),
+		catchError(this.handleError<Hero[]>('searchHeroes', []))
+	);
   }
 
   /** Log a HeroService message with the MessageService */
